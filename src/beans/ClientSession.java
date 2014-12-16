063747492ea4b439185aa2a576c2b2201c6b233b
Tony <CodeGuro@gmail.com>
2014-12-16 17:13:29 -0500

    progressing...

--- src/beans/ClientSession.java
+++ src/beans/ClientSession.java
@@ -1,5 +1,6 @@
 package beans;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.EJB;
@@ -17,10 +18,65 @@ import resources.Menu;
 @LocalBean
 public class ClientSession
 {
+	private class IngredientContainer
+	{
+		private Ingredient ingredient;
+		public boolean included = true;
+		public IngredientContainer( Ingredient ing )
+		{
+			this.ingredient = ing;
+		}
+		public Ingredient getIngredient()
+		{
+			return this.ingredient;
+		}
+	}
+	private class FoodContainer
+	{
+		private Food food;
+		private List< IngredientContainer > ingreds = new ArrayList< IngredientContainer >();
+		public boolean included = true;
+		public FoodContainer( Food food )
+		{
+			this.food = food;
+			for( Ingredient ingred : food.getIngredients() )
+				ingreds.add( new IngredientContainer( ingred ) );
+		}
+		private Food getFood()
+		{
+			return this.food;
+		}
+		private List< IngredientContainer > getIngredients()
+		{
+			return this.ingreds;
+		}
+	}
+	private class MenuContainer
+	{
+		private Menu menu;
+		private List< FoodContainer > foods = new ArrayList< FoodContainer >();
+		private Menu getMenu()
+		{
+			return menu;
+		}
+		public List< FoodContainer > getFoods()
+		{
+			return this.foods;
+		}
+		public MenuContainer( Menu menu )
+		{
+			this.menu = menu;
+			for( Food food : menu.getFoods() )
+			{
+				this.foods.add( new FoodContainer( food ) );
+			}
+		}
+	}
 	@EJB
 	private ResourceEAO resources;
 	
-	private Menu selectedMenu;
+	private MenuContainer selectedMenu;
+	private FoodContainer selectedFood;
 
 	public ResourceEAO getResources()
 	{
@@ -39,20 +95,60 @@ public class ClientSession
 
 	public Menu getSelectedMenu()
 	{
-		return selectedMenu;
+		return selectedMenu.getMenu();
 	}
 
 	public String setSelectedMenu( Menu menuItem )
 	{
 		if( menuItem == null )
 			return "failure";
-		this.selectedMenu = menuItem;
+		this.selectedMenu = new MenuContainer( menuItem );
+		
 		return "goToFoodCustomizationPage";
 	}
 	
 	public List< Food > getFoodsFromSelectedMenu()
 	{
-		return this.getSelectedMenu().getFoods();
+		List< Food > foods = new ArrayList< Food >();
+		for( FoodContainer fc : selectedMenu.getFoods())
+			if( fc.included )
+				foods.add( fc.getFood() );
+		return foods;
+	}
+	
+	public List< Food > getOmittedFoodsFromSelectedMenu()
+	{
+		List< Food > foods = new ArrayList< Food >();
+		for( FoodContainer fc : selectedMenu.getFoods())
+			if( !fc.included )
+				foods.add( fc.getFood() );
+		return foods;
+	}
+	
+	public String removeFood( Food food )
+	{
+		for( FoodContainer fc : selectedMenu.foods )
+		{
+			if( fc.getFood().getFoodid() == food.getFoodid() )
+			{
+				fc.included = false;
+				return "success";
+			}
+		}
+		return "failure";
+	}
+	
+	public String addFood( Food food )
+	{
+		for( FoodContainer fc : selectedMenu.foods )
+		{
+			if( fc.getFood().getFoodid() == food.getFoodid() )
+			{
+				fc.included = true;
+				return "success";
+			}
+		}
+		return "failure";
 	}
 	
 	public String getAllergensFor( Food food )
@@ -71,6 +167,51 @@ public class ClientSession
 		
 		if( result.length() == 0 )
 			result = "no allergens";
+		return result;
+	}
+
+	public Food getSelectedFood()
+	{
+		return selectedFood.getFood();
+	}
+
+	public String setSelectedFood( Food selectedFood )
+	{
+		for( FoodContainer ct : selectedMenu.getFoods() )
+		{
+			if( ct.getFood().getFoodid() == selectedFood.getFoodid() )
+			{
+				this.selectedFood = ct;
+				return "goToIngredientCustomizationPage";
+			}
+		}
+		
+		return "failure";
+	}
+	
+	public List< Ingredient > getIncludedIngredientsFromSelectedFood()
+	{
+		List< Ingredient > result = new ArrayList< Ingredient >();
+		
+		for( IngredientContainer ingred : selectedFood.getIngredients() )
+		{
+			if( ingred.included )
+				result.add( ingred.getIngredient() );
+		}
+		
+		return result;
+	}
+	
+	public List< Ingredient > getOmittedIngredientsFromSelectedFood()
+	{
+		List< Ingredient > result = new ArrayList< Ingredient >();
+		
+		for( IngredientContainer ingred : selectedFood.getIngredients() )
+		{
+			if( !ingred.included )
+				result.add( ingred.getIngredient() );
+		}
+		
 		return result;
 	}
 	
